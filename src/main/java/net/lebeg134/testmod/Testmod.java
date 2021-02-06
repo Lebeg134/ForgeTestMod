f18262035e6e270e95ad67eea8bab45b25a69dd3
Lebeg134 <cactusmoovies@gmail.com>
2021-02-06 15:03:54 +0100

    renamed entry point

---
+++ src/main/java/net/lebeg134/testmod/Testmod.java
@@ -0,0 +1,30 @@
+package net.lebeg134.testmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Testmod.MODID, name = Testmod.NAME, version = Testmod.VERSION)
+public class Testmod {
+    public static final String MODID = "testmod";
+    public static final String NAME = "TestMod";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}

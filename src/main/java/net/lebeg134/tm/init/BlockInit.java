25718961167d70878045f515ad38740870fb1613
Lebeg134 <cactusmoovies@gmail.com>
2021-02-18 17:13:44 +0100

    Added libs

--- src/main/java/net/lebeg134/tm/init/BlockInit.java
+++ src/main/java/net/lebeg134/tm/init/BlockInit.java
@@ -1,4 +1,10 @@
 package net.lebeg134.tm.init;
 
+import net.minecraft.block.Block;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockInit {
+    public static final List<Block> BLOCKS = new ArrayList<Block>();
 }
